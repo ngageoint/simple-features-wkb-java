b557297caf970a0783467d5512bf8d4cdbfe6279
Brian Osborn <bosborn@gmail.com>
2020-06-15 07:51:26 -0600

    finite filter test

--- src/test/java/mil/nga/sf/wkb/test/WKBTest.java
+++ src/test/java/mil/nga/sf/wkb/test/WKBTest.java
@@ -2,6 +2,10 @@ package mil.nga.sf.wkb.test;
 
 import java.io.IOException;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
 
 import junit.framework.TestCase;
 import mil.nga.sf.CompoundCurve;
@@ -18,10 +22,14 @@ import mil.nga.sf.Point;
 import mil.nga.sf.Polygon;
 import mil.nga.sf.Surface;
 import mil.nga.sf.extended.ExtendedGeometryCollection;
+import mil.nga.sf.util.ByteReader;
+import mil.nga.sf.util.ByteWriter;
 import mil.nga.sf.util.GeometryEnvelopeBuilder;
+import mil.nga.sf.util.filter.FiniteFilterType;
+import mil.nga.sf.util.filter.PointFiniteFilter;
 import mil.nga.sf.wkb.GeometryCodes;
-
-import org.junit.Test;
+import mil.nga.sf.wkb.GeometryReader;
+import mil.nga.sf.wkb.GeometryWriter;
 
 /**
  * Test Well Known Binary Geometries
@@ -113,20 +121,19 @@ public class WKBTest {
 				0, 0, 0, 3, 64, 50, -29, -55, -6, 126, -15, 120, -64, 65, -124,
 				-86, -46, -62, -60, 94, -64, 66, -31, -40, 124, -2, -47, -5,
 				-64, 82, -13, -22, 8, -38, 6, 111, 64, 81, 58, 88, 78, -15, 82,
-				111, -64, 86, 20, -18, -37, 3, -99, -86, 0, 0, 0, 0, 2, 0, 0,
-				0, 10, 64, 98, 48, -84, 37, -62, 34, 98, -64, 68, -12, -81,
-				104, 13, -109, 6, -64, 101, -82, 76, -68, 34, 117, -110, 64,
-				39, -125, 83, 1, 50, 86, 8, -64, 83, 127, -93, 42, -89, 54,
-				-56, -64, 67, -58, -13, -104, 1, -17, -10, 64, 97, 18, -82,
-				-112, 100, -128, 16, 64, 68, -13, -86, -112, 112, 59, -3, 64,
-				67, -4, -71, -91, -16, -15, 85, -64, 49, 110, -16, 94, -71, 24,
-				-13, -64, 94, 84, 94, -4, -78, -101, -75, -64, 80, 74, -39, 90,
-				38, 107, 104, 64, 72, -16, -43, 82, -112, -39, 77, 64, 28, 30,
-				97, -26, 64, 102, -110, 64, 92, 63, -14, -103, 99, -67, 63,
-				-64, 65, -48, 84, -37, -111, -55, -25, -64, 101, -10, -62,
-				-115, 104, -125, 28, -64, 66, 5, 108, -56, -59, 69, -36, -64,
-				83, 33, -36, -86, 106, -84, -16, 64, 70, 30, -104, -50, -57,
-				15, -7 };
+				111, -64, 86, 20, -18, -37, 3, -99, -86, 0, 0, 0, 0, 2, 0, 0, 0,
+				10, 64, 98, 48, -84, 37, -62, 34, 98, -64, 68, -12, -81, 104,
+				13, -109, 6, -64, 101, -82, 76, -68, 34, 117, -110, 64, 39,
+				-125, 83, 1, 50, 86, 8, -64, 83, 127, -93, 42, -89, 54, -56,
+				-64, 67, -58, -13, -104, 1, -17, -10, 64, 97, 18, -82, -112,
+				100, -128, 16, 64, 68, -13, -86, -112, 112, 59, -3, 64, 67, -4,
+				-71, -91, -16, -15, 85, -64, 49, 110, -16, 94, -71, 24, -13,
+				-64, 94, 84, 94, -4, -78, -101, -75, -64, 80, 74, -39, 90, 38,
+				107, 104, 64, 72, -16, -43, 82, -112, -39, 77, 64, 28, 30, 97,
+				-26, 64, 102, -110, 64, 92, 63, -14, -103, 99, -67, 63, -64, 65,
+				-48, 84, -37, -111, -55, -25, -64, 101, -10, -62, -115, 104,
+				-125, 28, -64, 66, 5, 108, -56, -59, 69, -36, -64, 83, 33, -36,
+				-86, 106, -84, -16, 64, 70, 30, -104, -50, -57, 15, -7 };
 
 		TestCase.assertEquals(GeometryCodes.getCode(GeometryType.MULTICURVE),
 				bytes[4]);
@@ -350,7 +357,8 @@ public class WKBTest {
 
 		// Test a pre-created WKB hex saved as a 2.5D MultiPolygon
 
-		byte[] bytes = hexStringToByteArray("0106000080010000000103000080010000000F0000007835454789C456C0DFDB63124D3F2C4000000000000000004CE4512E89C456C060BF20D13F3F2C400000000000000000A42EC6388CC456C0E0A50400423F2C400000000000000000B4E3B1608CC456C060034E67433F2C400000000000000000F82138508DC456C09FD015C5473F2C400000000000000000ECD6591B8CC456C000C305BC5B3F2C4000000000000000001002AD0F8CC456C060DB367D5C3F2C40000000000000000010996DEF8AC456C0BF01756A6C3F2C4000000000000000007054A08B8AC456C0806A0C1F733F2C4000000000000000009422D81D8AC456C041CA3C5B8A3F2C4000000000000000003CCB05C489C456C03FC4FC52AA3F2C400000000000000000740315A689C456C0BFC8635EB33F2C400000000000000000E4A5630B89C456C0DFE726D6B33F2C400000000000000000F45A4F3389C456C000B07950703F2C4000000000000000007835454789C456C0DFDB63124D3F2C400000000000000000");
+		byte[] bytes = hexStringToByteArray(
+				"0106000080010000000103000080010000000F0000007835454789C456C0DFDB63124D3F2C4000000000000000004CE4512E89C456C060BF20D13F3F2C400000000000000000A42EC6388CC456C0E0A50400423F2C400000000000000000B4E3B1608CC456C060034E67433F2C400000000000000000F82138508DC456C09FD015C5473F2C400000000000000000ECD6591B8CC456C000C305BC5B3F2C4000000000000000001002AD0F8CC456C060DB367D5C3F2C40000000000000000010996DEF8AC456C0BF01756A6C3F2C4000000000000000007054A08B8AC456C0806A0C1F733F2C4000000000000000009422D81D8AC456C041CA3C5B8A3F2C4000000000000000003CCB05C489C456C03FC4FC52AA3F2C400000000000000000740315A689C456C0BFC8635EB33F2C400000000000000000E4A5630B89C456C0DFE726D6B33F2C400000000000000000F45A4F3389C456C000B07950703F2C4000000000000000007835454789C456C0DFDB63124D3F2C400000000000000000");
 
 		TestCase.assertEquals(1, bytes[0]); // little endian
 		TestCase.assertEquals(GeometryCodes.getCode(GeometryType.MULTIPOLYGON),
@@ -399,6 +407,84 @@ public class WKBTest {
 	}
 
 	/**
+	 * Test geometry finite filtering
+	 * 
+	 * @throws Exception
+	 *             upon error
+	 */
+	@Test
+	public void testFiniteFilter() throws Exception {
+
+		Point point = WKBTestUtils.createPoint(false, false);
+
+		Point nan = new Point(Double.NaN, Double.NaN);
+		Point nanZ = WKBTestUtils.createPoint(true, false);
+		nanZ.setZ(Double.NaN);
+		Point nanM = WKBTestUtils.createPoint(false, true);
+		nanM.setM(Double.NaN);
+		Point nanZM = WKBTestUtils.createPoint(true, true);
+		nanZM.setZ(Double.NaN);
+		nanZM.setM(Double.NaN);
+
+		Point infinite = new Point(Double.POSITIVE_INFINITY,
+				Double.POSITIVE_INFINITY);
+		Point infiniteZ = WKBTestUtils.createPoint(true, false);
+		infiniteZ.setZ(Double.POSITIVE_INFINITY);
+		Point infiniteM = WKBTestUtils.createPoint(false, true);
+		infiniteM.setM(Double.POSITIVE_INFINITY);
+		Point infiniteZM = WKBTestUtils.createPoint(true, true);
+		infiniteZM.setZ(Double.POSITIVE_INFINITY);
+		infiniteZM.setM(Double.POSITIVE_INFINITY);
+
+		Point nanInfinite = new Point(Double.NaN, Double.POSITIVE_INFINITY);
+		Point nanInfiniteZM = WKBTestUtils.createPoint(true, true);
+		nanInfiniteZM.setZ(Double.NaN);
+		nanInfiniteZM.setM(Double.NEGATIVE_INFINITY);
+
+		Point infiniteNan = new Point(Double.POSITIVE_INFINITY, Double.NaN);
+		Point infiniteNanZM = WKBTestUtils.createPoint(true, true);
+		infiniteNanZM.setZ(Double.NEGATIVE_INFINITY);
+		infiniteNanZM.setM(Double.NaN);
+
+		LineString lineString = new LineString();
+		lineString.addPoint(point);
+		lineString.addPoint(nan);
+		lineString.addPoint(WKBTestUtils.createPoint(false, false));
+		lineString.addPoint(infinite);
+		lineString.addPoint(WKBTestUtils.createPoint(true, false));
+		lineString.addPoint(nanZ);
+		lineString.addPoint(WKBTestUtils.createPoint(false, true));
+		lineString.addPoint(infiniteZ);
+		lineString.addPoint(WKBTestUtils.createPoint(true, true));
+		lineString.addPoint(nanM);
+		lineString.addPoint(WKBTestUtils.createPoint(false, false));
+		lineString.addPoint(infiniteM);
+		lineString.addPoint(WKBTestUtils.createPoint(true, false));
+		lineString.addPoint(nanZM);
+		lineString.addPoint(WKBTestUtils.createPoint(false, true));
+		lineString.addPoint(infiniteZM);
+		lineString.addPoint(WKBTestUtils.createPoint(true, true));
+		lineString.addPoint(nanInfinite);
+		lineString.addPoint(WKBTestUtils.createPoint(false, false));
+		lineString.addPoint(infiniteNan);
+		lineString.addPoint(WKBTestUtils.createPoint(true, false));
+		lineString.addPoint(nanInfiniteZM);
+		lineString.addPoint(WKBTestUtils.createPoint(false, true));
+		lineString.addPoint(infiniteNanZM);
+
+		Polygon polygon = new Polygon();
+		polygon.addRing(lineString);
+
+		for (Point pnt : lineString.getPoints()) {
+			testFiniteFilter(pnt);
+		}
+
+		testFiniteFilter(lineString);
+		testFiniteFilter(polygon);
+
+	}
+
+	/**
 	 * Test the geometry writing to and reading from bytes
 	 * 
 	 * @param geometry
@@ -437,11 +523,9 @@ public class WKBTest {
 				ByteOrder.LITTLE_ENDIAN);
 		Geometry geometry2opposite = WKBTestUtils.readGeometry(bytes2,
 				ByteOrder.BIG_ENDIAN);
-		WKBTestUtils.compareByteArrays(
-				WKBTestUtils.writeBytes(compareGeometry),
+		WKBTestUtils.compareByteArrays(WKBTestUtils.writeBytes(compareGeometry),
 				WKBTestUtils.writeBytes(geometry1opposite));
-		WKBTestUtils.compareByteArrays(
-				WKBTestUtils.writeBytes(compareGeometry),
+		WKBTestUtils.compareByteArrays(WKBTestUtils.writeBytes(compareGeometry),
 				WKBTestUtils.writeBytes(geometry2opposite));
 
 		Geometry geometry1 = WKBTestUtils.readGeometry(bytes1,
@@ -475,10 +559,126 @@ public class WKBTest {
 		int len = hex.length();
 		byte[] bytes = new byte[len / 2];
 		for (int i = 0; i < len; i += 2) {
-			bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character
-					.digit(hex.charAt(i + 1), 16));
+			bytes[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+					+ Character.digit(hex.charAt(i + 1), 16));
 		}
 		return bytes;
+	}
+
+	/**
+	 * Test fine filter for the geometry
+	 * 
+	 * @param geometry
+	 *            geometry
+	 * @throws Exception
+	 */
+	private void testFiniteFilter(Geometry geometry) throws Exception {
+
+		ByteWriter writer = new ByteWriter();
+		GeometryWriter.writeGeometry(writer, geometry);
+		byte[] bytes = writer.getBytes();
+
+		testFiniteFilter(bytes, new PointFiniteFilter());
+		testFiniteFilter(bytes, new PointFiniteFilter(true));
+		testFiniteFilter(bytes, new PointFiniteFilter(false, true));
+		testFiniteFilter(bytes, new PointFiniteFilter(true, true));
+		testFiniteFilter(bytes,
+				new PointFiniteFilter(FiniteFilterType.FINITE_AND_NAN));
+		testFiniteFilter(bytes,
+				new PointFiniteFilter(FiniteFilterType.FINITE_AND_NAN, true));
+		testFiniteFilter(bytes, new PointFiniteFilter(
+				FiniteFilterType.FINITE_AND_NAN, false, true));
+		testFiniteFilter(bytes, new PointFiniteFilter(
+				FiniteFilterType.FINITE_AND_NAN, true, true));
+		testFiniteFilter(bytes,
+				new PointFiniteFilter(FiniteFilterType.FINITE_AND_INFINITE));
+		testFiniteFilter(bytes, new PointFiniteFilter(
+				FiniteFilterType.FINITE_AND_INFINITE, true));
+		testFiniteFilter(bytes, new PointFiniteFilter(
+				FiniteFilterType.FINITE_AND_INFINITE, false, true));
+		testFiniteFilter(bytes, new PointFiniteFilter(
+				FiniteFilterType.FINITE_AND_INFINITE, true, true));
+
+	}
+
+	/**
+	 * Filter and validate the geometry bytes
+	 * 
+	 * @param bytes
+	 *            geometry bytes
+	 * @param filter
+	 *            point finite filter
+	 */
+	private void testFiniteFilter(byte[] bytes, PointFiniteFilter filter) {
+
+		ByteReader reader = new ByteReader(bytes);
+		Geometry geometry = GeometryReader.readGeometry(reader, filter);
+
+		if (geometry != null) {
+
+			List<Point> points = new ArrayList<>();
+
+			switch (geometry.getGeometryType()) {
+			case POINT:
+				points.add((Point) geometry);
+				break;
+			case LINESTRING:
+				points.addAll(((LineString) geometry).getPoints());
+				break;
+			case POLYGON:
+				points.addAll(((Polygon) geometry).getRing(0).getPoints());
+				break;
+			default:
+				TestCase.fail(
+						"Unexpected test case: " + geometry.getGeometryType());
+			}
+
+			for (Point point : points) {
+
+				switch (filter.getType()) {
+				case FINITE:
+					TestCase.assertTrue(Double.isFinite(point.getX()));
+					TestCase.assertTrue(Double.isFinite(point.getY()));
+					if (filter.isFilterZ() && point.hasZ()) {
+						TestCase.assertTrue(Double.isFinite(point.getZ()));
+					}
+					if (filter.isFilterM() && point.hasM()) {
+						TestCase.assertTrue(Double.isFinite(point.getM()));
+					}
+					break;
+				case FINITE_AND_NAN:
+					TestCase.assertTrue(Double.isFinite(point.getX())
+							|| Double.isNaN(point.getX()));
+					TestCase.assertTrue(Double.isFinite(point.getY())
+							|| Double.isNaN(point.getY()));
+					if (filter.isFilterZ() && point.hasZ()) {
+						TestCase.assertTrue(Double.isFinite(point.getZ())
+								|| Double.isNaN(point.getZ()));
+					}
+					if (filter.isFilterM() && point.hasM()) {
+						TestCase.assertTrue(Double.isFinite(point.getM())
+								|| Double.isNaN(point.getM()));
+					}
+					break;
+				case FINITE_AND_INFINITE:
+					TestCase.assertTrue(Double.isFinite(point.getX())
+							|| Double.isInfinite(point.getX()));
+					TestCase.assertTrue(Double.isFinite(point.getY())
+							|| Double.isInfinite(point.getY()));
+					if (filter.isFilterZ() && point.hasZ()) {
+						TestCase.assertTrue(Double.isFinite(point.getZ())
+								|| Double.isInfinite(point.getZ()));
+					}
+					if (filter.isFilterM() && point.hasM()) {
+						TestCase.assertTrue(Double.isFinite(point.getM())
+								|| Double.isInfinite(point.getM()));
+					}
+					break;
+				}
+
+			}
+		}
+
 	}
 
 }
